2baf3df55873cb5781002acaf8dd00b8f7dd9d52
Scott Page <scott@mytastebud.com>
2015-05-14 21:12:47 -0500

    Work In Progress.
    Need to figure out how to split by first :
    
    then this thing will be able to make a nice data frame for us

--- get.R
+++ get.R
@@ -1,1 +1,14 @@
 # Gather Data with functions here
+library(dplyr)
+library(tidyr)
+
+get_movie_reviews <- function(){
+  data <- readLines("data/movies.txt", n = 10)
+  data <- data.frame(text = data) 
+  data$text <- as.character(data$text)
+  data <- filter(data, text != "")
+  data <- data %>%
+            separate(text, c("key", "value"), sep = "^[^:]+:\\s*", error = "drop")
+  
+  gsub("/^(.+?):", "!@!", data$text) # This needs help.
+}
