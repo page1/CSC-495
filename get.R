243982a75cf3d90b17b7f7c55f664e55a79c95bd
Scott Page <scott@mytastebud.com>
2015-05-15 20:38:32 -0500

    spread data
    Have an error in a record if you run for 10000 lines. But this is
    progress

--- get.R
+++ get.R
@@ -2,22 +2,25 @@
 library(dplyr)
 library(tidyr)
 
-get_movie_reviews <- function(){
-  data <- readLines("data/movies.txt", n = 1000)
+get_movie_reviews <- function(n = 1000){
+  data <- readLines("data/movies.txt", n = n)
   data <- data.frame(text = data) 
   data <- data %>%
             mutate(text = as.character(text)) %>%
-            filter(nchar(text) > 0)
-
-  d <- data %>%
-        extract(text, into=c('key', 'value'), '(.*):\\s+(.*)+')
+            filter(nchar(text) > 0) %>%
+            extract(text, into=c('key', 'value'), '(.*):\\s+(.*)+')
   
   index <- 0
-  d$id <- 0
-  for(i in 1:nrow(d)){
-    if(d$key[[i]] == "product/productId"){
+  data$id <- 0
+  for(i in 1:nrow(data)){
+    if(data$key[[i]] == "product/productId"){
       index <- index + 1
     }
-    d$id[[i]] <- index
+    data$id[[i]] <- index
   }
+  
+  data <- data %>%
+        spread(key, value)
+  
+  return(data)
 }
