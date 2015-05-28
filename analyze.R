3cf9c32fca12578f36ac45f02b2ebb5efd58c602
Tim <tsachs000@gmail.com>
2015-05-28 09:50:53 -0500

    Updated analyze.R
    Added in the par() function so all the graphs are together and also changed the breaks parameter to 100 so there are more bars in the bar graph to help the visualization

--- analyze.R
+++ analyze.R
@@ -2,15 +2,20 @@
 library(sand)
 
 make_plots <- function(graph){
+  par(mfrow=c(2,2))
   hist(degree(graph),
        main = "Degree Distribution",
-       xlab = "Degree")
+       xlab = "Degree",
+       breaks = 100)
   
   hist(graph.strength(graph),
        main = "Graph Strength Distribution",
-       xlab = "Graph Strength")
+       xlab = "Graph Strength",
+       breaks = 100)
   
   hist(evcent(graph)$vector,
        main = "Eigenvector Centrality Distribution",
-       xlab = "Eigenvector")
+       xlab = "Eigenvector",
+       breaks = 100)
+  par(mfrow=c(1,1))
 }
