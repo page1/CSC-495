52fbd8343413ac343de8fd2abcdc491d2b4ac3c4
Scott Page <scott@mytastebud.com>
2015-05-27 22:52:44 -0500

    Consume the reduced review data
    The main file will work off of the uncompressed version of
    movies.min.csv that you should put in the data folder.
    
    This update will also cache things into Rdata files to make re running
    faster.
    
    This code is very memory intensive, but runs to completion on my 8gb
    ram mac book
    
    The output file in grapheme is 12 gb
    
    it has yet to be seen if it can be opened in guff

--- do.R
+++ do.R
@@ -1,1 +1,21 @@
 # Run all the other files with functions here
+source("get.R")
+source("munge.R")
+library(sand)
+
+main <- function(){
+  Rdata_file <- "data/movie_movie_projection.Rdata"
+  
+  if(file.exists(Rdata_file)){
+    load(Rdata_file)
+  } else {
+    data <- get_min_movie_reviews()
+    full_graph <- make_review_graph(data)
+    projection <- make_movie_movie_projection(full_graph)
+    save(projection, file = Rdata_file)
+  }
+  
+  write.graph(projection, file = "data/movie_movie_projection.graphml", format = "graphml")
+  
+  return(NULL)
+}
--- get.R
+++ get.R
@@ -1,27 +1,80 @@
 # Gather Data with functions here
 library(dplyr)
 library(tidyr)
+library(stringr)
+
+get_min_movie_reviews <- function(){
+  Rdata_file <- "data/movies.min.Rdata"
+  
+  if(file.exists(Rdata_file)){
+    load(Rdata_file)
+  } else {
+    data <- read.csv(file = "data/movies.min.csv")
+    save(data, file = Rdata_file)
+  }
+  
+  return(data)
+}
 
 get_movie_reviews <- function(n = 1000){
-  data <- readLines(con <- gzfile("data/movies.txt.gz", encoding = "latin1"), n = n)
+  file <- "data/movies.txt"
+  con <- file(description = file, encoding = "latin1", open="r")
+  
+  ## Hopefully you know the number of lines from some other source or
+  com <- paste("wc -l ", file, " | awk '{ print $1 }'", sep="")
+  n <- system(command=com, intern=TRUE)
+  n <- as.numeric(n)
+  
+  #n <- 9 * 100000
+  #vec <- rep(character(0), times = (n / 9) * 5)
+  #index <- 1
+
+  last_time <- Sys.time()
+  records <- (n / 9)
+  vec <- lapply(1:records, function(i) {
+    tmp <- scan(file=con, nlines=9, quiet=TRUE, what = character(), sep = "\n")
+
+    if(i %% 1000 == 0){
+      elapsed <- Sys.time() - last_time
+      msg_per_sec <- 1000/as.numeric(elapsed, units = "secs")
+      eta_minutes <- (records - i) / msg_per_sec / 60
+      last_time <<- Sys.time()
+      print(paste(i, "out of", records, "Running at", round(msg_per_sec, digits = 1), "per second, eta = ", round(eta_minutes, digits = 1), "minutes"))
+    }
+    
+    return(tmp[c(1,2,4,5,6)])
+  })
+
+ vec <- unlist(vec)
   close(con)
-  data <- data.frame(text = data) 
+  
+ save(vec, file = "vec.Rdata")
+ load("vec.Rdata")
+ vec <- str_match(data$text, '(.*):\\s+(.*)')
+ save(vec, file = "vec_split.Rdata")
+  
+  data <- data.frame(vec[,2:3])
+ 
   data <- data %>%
             mutate(text = as.character(text)) %>%
             filter(nchar(text) > 0) %>%
             extract(text, into=c('key', 'value'), '(.*):\\s+(.*)+')
-  
+ 
+ save(data, file = "data1.Rdata")
+ 
   index <- 0
-  data$id <- 0
-  for(i in 1:nrow(data)){
-    if(data$key[[i]] == "product/productId"){
-      index <- index + 1
-    }
-    data$id[[i]] <- index
-  }
-  
+  data$id <- sapply(data$key, function(key){
+               if(key == "product/productId"){
+                 index <<- index + 1
+               }
+               return(index)
+              })
+ 
+ save(data, file = "data2.Rdata")
+ 
   data <- data %>%
         spread(key, value)
+ save(data, file = "final_data.Rdata")    
   
   return(data)
 }
